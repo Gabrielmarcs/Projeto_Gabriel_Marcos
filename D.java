4942ebc0d3a0be768e0e0f633d282d2d42037410
Gabriel Marcos Lopes <gabriel10svm7@gmail.com>
2023-10-02 13:50:03 -0300

    Criado Classe D

---
+++ D.java
@@ -0,0 +1,28 @@
+public class D {
+    private String d1;
+    private int d2;
+
+    public String getD1(){
+        return this.d1;
+    }
+
+    public int getD2(){
+        return this.d2;
+    }
+
+    public void setD1(String d1){
+        this.d1 = d1;
+    }
+
+    public void setD2(int d2){
+        this.d2 = d2;
+    }
+
+    public void md1(){
+        System.out.println("MD1");
+    }
+
+    public void md2(){
+        System.out.println("MD2");
+    }
+}
