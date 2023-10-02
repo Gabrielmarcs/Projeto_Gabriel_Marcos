9edd77c00d4720b38e1b4a955d929502681c740a
Gabriel Marcos Lopes <gabriel10svm7@gmail.com>
2023-10-02 13:25:43 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,23 @@
+public class C {
+    private String c1;
+    private int c2;
+
+    public String getC1(){
+        return this.c1;
+    }
+    public int getC2(){
+        return this.c2;
+    }
+    public void setC1(String c1){
+        this.c1 = c1;
+    }
+    public void setC2(int c2){
+        this.c2 = c2;
+    }
+    public void mc1(){
+        System.out.println("MC1");
+    }
+    public void mc2(){
+        System.out.println("MC2");
+    }
+}
